a7857a8deaf9f1678873d661e26f58c433b75741
‘cmy’ <3036319203@qq.com>
2024-06-27 16:26:51 +0800

    init repositoty

---
+++ src/main/java/com/example/communitytest/CommunityTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.communitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/communitytest/CommunityTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.communitytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
